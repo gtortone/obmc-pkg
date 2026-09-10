c1a9a3e691b50b95ab2cb76bb3d618a2dc0141bb
Gennaro Tortone <gennaro.tortone@na.infn.it>
2026-09-10 13:56:36 +0200

    add scratch directory in container

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apptainer exec almalinux10.sif scripts/run.sh
+#apptainer exec almalinux10.sif scripts/run.sh
 
-
+apptainer exec --bind /scratch:/scratch almalinux10.sif scripts/run.sh
--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-apptainer exec almalinux10.sif /bin/bash 
+#apptainer exec almalinux10.sif /bin/bash 
 
+apptainer exec --bind /scratch:/scratch almalinux10.sif /bin/bash
 
